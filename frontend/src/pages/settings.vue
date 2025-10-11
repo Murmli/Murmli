01b7589291e496be2c181fa01c8c727725e310b0
SphaeroX <sphaerox@gmail.com>
2025-10-11 13:59:22 +0200

    create global share component and add to settings/help

--- frontend/src/pages/settings.vue
+++ frontend/src/pages/settings.vue
@@ -8,11 +8,6 @@
           <!-- Sprache -->
           <LanguageSelect />
           <StartpageSelect />
-          <v-row class="justify-center text-center mt-4 mb-6">
-            <v-btn color="primary" variant="tonal" prepend-icon="mdi-help-circle-outline" @click="goHelp">
-              {{ languageStore.t('helpPage.openHelpCenter') }}
-            </v-btn>
-          </v-row>
           <div class="justify-center text-center">
             <UserIdDisplay class="pb-5" />
 
@@ -60,47 +55,7 @@
 
     <v-divider class="my-6"></v-divider>
 
-    <!-- App: Rate and Share Links -->
-    <v-row class="d-flex justify-center mb-3">
-      <v-chip class="px-4" color="primary" text-color="white" variant="elevated" prepend-icon="mdi-heart"
-        @click="openLink('https://play.google.com/store/apps/details?id=de.murmli.twa')">
-        {{ languageStore.t('navigation.rateApp') }}
-      </v-chip>
-    </v-row>
-
-    <v-row class="d-flex justify-center">
-      <v-btn icon variant="text" @click="shareApp">
-        <v-tooltip :text="languageStore.t('navigation.shareApp')" location="top">
-          <template #activator="{ props }">
-            <v-icon v-bind="props">mdi-share-variant-outline</v-icon>
-          </template>
-        </v-tooltip>
-      </v-btn>
-
-      <v-btn icon variant="text" @click="openLink('mailto:prompt-engineered@protonmail.com?subject=Murmli%20App')">
-        <v-tooltip :text="languageStore.t('navigation.email')" location="top">
-          <template #activator="{ props }">
-            <v-icon v-bind="props" color="cyan-darken-2">mdi-email-outline</v-icon>
-          </template>
-        </v-tooltip>
-      </v-btn>
-
-      <v-btn icon variant="text" @click="openLink('https://discord.com/invite/qkxjGEp3Tg')">
-        <v-tooltip :text="'Discord'" location="top">
-          <template #activator="{ props }">
-            <v-icon v-bind="props" color="deep-purple-accent-2">mdi-chat</v-icon>
-          </template>
-        </v-tooltip>
-      </v-btn>
-
-      <v-btn icon variant="text" @click="openLink('https://www.reddit.com/r/Murmli/')">
-        <v-tooltip :text="'Reddit'" location="top">
-          <template #activator="{ props }">
-            <v-icon v-bind="props" color="red">mdi-reddit</v-icon>
-          </template>
-        </v-tooltip>
-      </v-btn>
-    </v-row>
+    <AppPromotionActions />
   </v-container>
 </template>
 
@@ -112,36 +67,16 @@
 
 <script setup>
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 import { useLanguageStore } from "@/stores/languageStore";
 import { useUserStore } from "@/stores/userStore";
 import AdminStats from "@/components/general/AdminStats.vue";
 import AdminTools from "@/components/general/AdminTools.vue";
+import AppPromotionActions from "@/components/general/AppPromotionActions.vue";
 
 const languageStore = useLanguageStore();
 const userStore = useUserStore();
-const router = useRouter();
 userStore.fetchRole();
 
 const panel = ref(0);
 const deleteDialog = ref(false);
-
-// Link helpers
-const openLink = (url) => {
-  window.open(url, '_blank')
-}
-
-const shareApp = () => {
-  const playStoreUrl = 'https://play.google.com/store/apps/details?id=de.murmli.twa'
-  if (navigator.share) {
-    navigator.share({ title: 'Murmli', url: playStoreUrl })
-  } else {
-    navigator.clipboard.writeText(playStoreUrl)
-    alert('Copied to the clipboard!')
-  }
-}
-
-const goHelp = () => {
-  router.push('/help')
-}
 </script>
