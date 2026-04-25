f54130090921b0c23f5c88814ef3431b0239d057
SphaeroX <sphaerox@gmail.com>
2026-04-25 04:42:27 +0200

    feat(i18n): use general yes/no translations and update locales

--- frontend/src/components/dialogs/calorieGoalDialog.vue
+++ frontend/src/components/dialogs/calorieGoalDialog.vue
@@ -162,8 +162,8 @@
             </v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn variant="text" @click="confirmReset(false)">{{ languageStore.t('tracker.dietLevelChange.resetNo') }}</v-btn>
-                <v-btn variant="text" color="primary" @click="confirmReset(true)">{{ languageStore.t('tracker.dietLevelChange.resetYes') }}</v-btn>
+                <v-btn variant="text" @click="confirmReset(false)">{{ languageStore.t('general.no') }}</v-btn>
+                <v-btn variant="text" color="primary" @click="confirmReset(true)">{{ languageStore.t('general.yes') }}</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
